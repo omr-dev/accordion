fbdf37fd7eb25f368d5f4b039060d2766c4f8d11
omer <oemer.peynirci@ontavio.de>
2022-11-21 21:55:04 +0100

    style finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,55 @@
-
-import './App.css'
+import './App.css';
 
 function App() {
 
-  return (
-    <div className="App">
-     <h1>Accordion</h1>
-    </div>
-  )
+    return (
+        <div className="App">
+            <div className="container">
+                <div className="up-left-box"/>
+                <div className="down-right-boxes">
+                    <div className="down-right-purple-inner-box"/>
+                </div>
+                <div className="main-box">
+                    <div className="main-box-title">
+                        <h1>Questions and answers about login</h1>
+                    </div>
+                    <div className="main-box-questions">
+                        <div className="questions-box-inner">
+                            <div className="left-side">
+                                <h4>Do I have to allow the use of cookies?</h4>
+                                <p>Unicorn vinyl poutine brooklyn,
+                                    next level direct trade iceland.
+                                    Shaman copper mug church-key coloring book,
+                                    whatever poutine normcore fixie cred kickstarter
+                                    post-ironic street art.</p>
+                            </div>
+                            <div className="right-side">-</div>
+                        </div>
+                        <div className="questions-box-inner">
+                            <div className="left-side">
+                                <h4>How do I change my My Page Password?</h4>
+
+                            </div>
+                            <div className="right-side">+</div>
+                        </div>
+                        <div className="questions-box-inner">
+                            <div className="left-side">
+                                <h4> What is BankID?</h4>
+
+                            </div>
+                            <div className="right-side">+</div>
+                        </div>
+
+
+
+
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
